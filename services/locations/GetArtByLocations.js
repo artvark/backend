c72dd07a5ac666628378d4474cc4e4fb2c7a1e62
luisgzz <lgpcollege@gmail.com>
2019-10-26 16:48:27 -0400

    clean up

--- services/locations/GetArtByLocations.js
+++ services/locations/GetArtByLocations.js
@@ -1,43 +1,6 @@
 import models from "../MongoConnect"
 
-
-
-const generatePoints = () => {
-    let lat = 33.777156
-    let lng = -84.396202
-
-    var r = 200/111300 // = 100 meters
-    var y0 = lat
-    var x0 = lng
-
-    for (var i = 0; i < 200; i++) {
-        var u = Math.random()
-        var v = Math.random()
-        var w = r * Math.sqrt(u)
-        var t = 2 * Math.PI * v
-        var x = w * Math.cos(t)
-        var y1 = w * Math.sin(t)
-        var x1 = x / Math.cos(y0)
-        var newY = y0 + y1
-        var newX = x0 + x1
-
-        console.log(newX)
-        console.log(newY)
-
-
-        var p = new models.Art({ 
-            name: "Point " + i, 
-            location: {
-                type: "Point",
-                coordinates: [newX, newY]
-            }
-        });
-        
-        p.save()
-    }
-}
-
-const getArtbyLatLng = (req, res, next) => {
+const getArtbyLngLat = (req, res, next) => {
 
     let search_lat = req.query.lat;
     let search_lng = req.query.lng;
@@ -61,5 +24,5 @@ const getArtbyLatLng = (req, res, next) => {
 
 }
 
-export {generatePoints, getArtbyLatLng}
+export {getArtbyLatLng}
 
