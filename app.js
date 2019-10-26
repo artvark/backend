0f343528998d408799fe7a35316c04870d63fe47
luisgzz <lgpcollege@gmail.com>
2019-10-26 16:41:11 -0400

    clean up

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.get('/', (req, res) => res.send('Hello World!'));
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-// import { generatePoints } from './services/locations/GetArtByLocations';
+// import { generatePoints } from './Utilities';
 // generatePoints()
--- dumps.js
+++
@@ -1,9 +0,0 @@
-blobService.createBlockBlobFromLocalFile('artwork', 'artvark', 'artvark.png', function(error, result, response) {
-    if (!error) {
-        console.log("uploaded")
-    }
-
-    if (error) {
-        console.log(error)
-    }
-});
